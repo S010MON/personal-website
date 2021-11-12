c469d7b75bb78d3b0d8f422e108da59122d747f1
S010MON <leon@bdebnath.com>
2021-11-12 16:49:16 +0100

    implements POST method

--- main.py
+++ main.py
@@ -2,33 +2,36 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-# these are GET functions
+# GET function: Runs hello world on your local host
 @app.route("/")
 def hello_world():
     return "<p>This is a test for Hello, World!</p>"
 
 
-# GET function
+# GET function: Adding a sub-level to the root of the URL
 @app.route("/about/")
 def about():
     return "<p>About the author</p>"
 
 
-# GET function
+# GET function: Taking in a parameter using the URL
 @app.route("/square/<number>", methods=['GET'])
 def square(number):
-    number = int(number)
+    number = int(number)        # This step is required to change the type to an Integer
     number = number * number
     return "<p>" + str(number) + "</p>"
 
 
-# Post function
-@app.route("/users", methods=['POST'])
+# Users Functionality
+@app.route("/users/", methods=['POST'])
 def users():
-    data = request.form
-    first_name = data["first_name"]
-    last_name = data["last_name"]
-    return "<p>new_user_added" + first_name + last_name + "</p>"
+
+    # POST method for
+    if request.method == 'POST':
+        first_name = request.json['first_name']
+        last_name = request.json['last_name']
+        return "<p>new_user_added " + first_name + " " + last_name + "</p>"
+
 
 if __name__ == "__main__":
     app.run()
