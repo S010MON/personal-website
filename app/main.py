ddfcfe2b27a1fe80c1cfcbdc7948a7500d626729
S010MON <10490509+S010MON@users.noreply.github.com>
2022-01-18 14:51:56 +0100

    adds forms

--- app/main.py
+++ app/main.py
@@ -1,10 +1,29 @@
-from fastapi import FastAPI
+import uvicorn
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates/")
 
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 async def root():
-    return {"<b> This is Leon's Website </b>"}
+    return "home.html"
 
+
+@app.get("/form")
+def form_post(request: Request):
+    result = "Type a number"
+    return templates.TemplateResponse('form.html', context={'request': request, 'result': result})
+
+
+@app.post("/form")
+def form_post(request: Request, num: int = Form(...)):
+    result = num
+    return templates.TemplateResponse('form.html', context={'request': request, 'result': result})
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
